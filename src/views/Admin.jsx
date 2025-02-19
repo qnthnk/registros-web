8eeef163eed5b3381b4d568f18247a52e8b5cd40
regenerik <regenerik@gmail.com>
2025-02-18 21:13:47 -0300

    faltaba useEffect

--- src/views/Admin.jsx
+++ src/views/Admin.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom';
 import './Admin.css';
 import Navbar from '../components/Navbar.jsx'
