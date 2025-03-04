fdd2d7d78ee1a1373e9096d34a72a91d94d2dbc0
Ruben <167398241+rumacon@users.noreply.github.com>
2025-03-04 19:24:28 +0000

    resize create

--- src/components/CardFront.jsx
+++ src/components/CardFront.jsx
@@ -5,28 +5,28 @@ const CardFront = ({ data, localImage }) => {
   console.log(data);
   return (
     <>
-    <div className="card-front">
-      <div className="cardContainer creditCard">
-        <div className="card-photo">
-          {data.url_image_self_photo ? (
-            <img src={localImage} alt="Foto" />
-          ) : (
-            <div className="placeholder">Foto</div>
-          )}
-        </div>
-        <div className="creditCardFront">
-        <p className="cardHolderLNF">{data.lastname_f || "A.Paterno"}</p>
+      <div className="card-front">
+        <div className="cardContainer creditCard">
+          <div className="card-photo">
+            {data.url_image_self_photo ? (
+              <img src={localImage} alt="Foto" />
+            ) : (
+              <div className="placeholder">Foto</div>
+            )}
+          </div>
+          <div className="creditCardFront">
+            <p className="cardHolderLNF">{data.lastname_f || "A.Paterno"}</p>
             <p className="cardHolderLNM">{data.lastname_m || "A.Materno"}</p>
             <p className="cardHolderN">{data.name || "Nombre"}</p>
-            <p className="cardHolderC" style={{maxWidth:"5px"}}>{data.org || "Estructura"}</p>
-            <p className="cardHolderO" style={{maxWidth:"5px"}}>{data.curp || "CURP"}</p>
+            <p className="cardHolderO">{data.org || "Estructura"}</p>
+            <p className="cardHolderC">{data.curp || "CURP"}</p>
             <p className="cardHolderEnt">{data.entidad_dir || "Entidad"}</p>
-            <p className="cardHolderExp">Expedición</p>
-            <p className="cardHolderVig">Vigencia</p>
+            <p className="cardHolderExp"></p>
+            <p className="cardHolderVig"></p>
 
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
