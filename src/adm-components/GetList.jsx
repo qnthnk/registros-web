8a29f0842731c2039afa6d65765cc8c2624d6a88
regenerik <regenerik@gmail.com>
2025-03-05 05:58:42 -0300

    image faltante

--- src/adm-components/GetList.jsx
+++ src/adm-components/GetList.jsx
@@ -1,11 +1,12 @@
-import React, {useState, useContext} from 'react'
+import React, { useState, useContext } from 'react';
 import { Context } from '../js/store/appContext.js';
+import './GetList.css';
 
 const GetList = () => {
-    const [isDownloading, setIsDownloading] = useState(false);
-    const {  actions } = useContext(Context)
+  const [isDownloading, setIsDownloading] = useState(false);
+  const { actions } = useContext(Context);
 
-      // Handler para descargar Excel
+  // Handler para descargar Excel
   const handleDownloadExcel = async () => {
     setIsDownloading(true);
     try {
@@ -16,23 +17,24 @@ const GetList = () => {
       setIsDownloading(false);
     }
   };
-    return (
-        <div>
 
-            {/* Botón para descargar Excel */}
-            <div className="download-excel-container">
-                <button className="btn download-excel-btn-blue" onClick={handleDownloadExcel} disabled={isDownloading}>
-                    {isDownloading ? (
-                        <>
-                            <i className="fas fa-spinner fa-spin"></i> Descargando...(puede demorar)
-                        </>
-                    ) : (
-                        "Descargar Excel Total"
-                    )}
-                </button>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="getlist-container">
+      <div className="download-excel-container">
+        <button className="btn download-excel-btn-blue" onClick={handleDownloadExcel} disabled={isDownloading}>
+          {isDownloading ? (
+            <>
+              <i className="fas fa-spinner fa-spin"></i> Descargando...(puede demorar)
+            </>
+          ) : (
+            <>
+              Descargar Excel Total <i className="fas fa-arrow-down"></i>
+            </>
+          )}
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default GetList
+export default GetList;
