a2a4cb23aca8356b0b85fab7fb53ccfdce22f11f
rumacon <rumacon@gmail.com>
2025-06-13 12:36:02 -0600

    se le agrega cuponera de chopo al home

--- src/views/HomeCNC.jsx
+++ src/views/HomeCNC.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import './HomeCNC.css';
+import Cuponera from './../img/cupon.jpg'
 
 
 import BackgroundVideo from "../img/CNClogin.gif";
+
+const cuponeraStyle = {
+  width: "100%",
+  height: "auto",
+  display: "block",
+  maxWidth: "100%", // asegura que nunca sobrepase el contenedor
+};
 
 const HomeCNC = () => {
 
 
   return (
+    <>
     <div className="background-video-container">
       <img src={BackgroundVideo} alt="Background Video" className="background-video" />
     </div>
+    <div className="contentVideo" style={{ overflow: "hidden" }}>
+        <div className="containerLI" style={{ overflow: "hidden" }}>
+          <h3 className="titleCNC">Cupones del mes</h3>
+        <img src={Cuponera} alt="Cuponera" style={cuponeraStyle} />
+        </div>
+        </div>
+
+    </>
 
 
   );
